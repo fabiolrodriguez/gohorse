d319a54cfc685e0206feb1589c9d4d4c8161b8b2
fabio.lima@byseven.com.br <fabio.lima@byseven.com.br>
2021-12-05 18:18:42 -0300

    aula 9+

--- 9-Arrays-Slices/array-slice.go
+++ 9-Arrays-Slices/array-slice.go
@@ -43,4 +43,28 @@ func main() {
 
 	fmt.Println(slice2)
 
+	// Array Interno
+
+	fmt.Println("-----------------------------------")
+
+	slice3 := make([]float32, 10, 11)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //tamanho
+	fmt.Println(cap(slice3)) //capacidade
+
+	slice3 = append(slice3, 5)
+
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //tamanho
+	fmt.Println(cap(slice3)) //capacidade
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
 }
